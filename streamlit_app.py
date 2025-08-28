afaa7cdaedaef5e31e45dc80393e92fbba6dd5ab
Lula2025 <rortega_mdl@hotmail.com>
2025-08-28 13:39:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,7 +132,7 @@ if seleccion_categorias and not todos_categorias:
     datos_filtrados = datos_filtrados[datos_filtrados["Categoria_Proyecto"].isin(seleccion_categorias)]
     
 st.markdown("---")  # Esta es la línea de separación
-st.sidebar.markdown('<hr style="border:1.5px dashed #444; margin:15px 0;">', unsafe_allow_html=True)
+st.sidebar.markdown('<hr style="border:1.5px dashed #000080; margin:15px 0;">', unsafe_allow_html=True)
 
 # --- Filtro por Proyecto ---
 proyectos = sorted(datos_filtrados["Proyecto"].unique())
